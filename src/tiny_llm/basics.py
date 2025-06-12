42e8410a276acff26f3e00452874397934911bf1
zxreaper <zxreaper@foxmail.com>
2025-06-12 21:36:15 +0800

    finish week_1_day_1_task_2. tests/week_1_day_1_task_2.py passed.

--- src/tiny_llm/basics.py
+++ src/tiny_llm/basics.py
@@ -12,7 +12,9 @@ def linear(
     w: mx.array,
     bias: mx.array | None = None,
 ) -> mx.array:
-    pass
+    if bias is None:
+        return mx.matmul(x, w.T)
+    return mx.matmul(x, w.T) + bias
 
 
 def silu(x: mx.array) -> mx.array:
